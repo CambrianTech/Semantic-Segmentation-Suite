bd1292cee1131122f07cc4a31df0d2d448a8fbcd
joelteply <joel@cambriantech.com>
2018-08-25 20:32:30 -0500

    data_augmentation not the problem

--- main.py
+++ main.py
@@ -138,21 +138,21 @@ def data_augmentation(input_image, output_image):
     # Data augmentation
     input_image, output_image = utils.random_crop(input_image, output_image, args.crop_height, args.crop_width)
 
-    # if args.h_flip and random.randint(0,1):
-    #     input_image = cv2.flip(input_image, 1)
-    #     output_image = cv2.flip(output_image, 1)
-    # if args.v_flip and random.randint(0,1):
-    #     input_image = cv2.flip(input_image, 0)
-    #     output_image = cv2.flip(output_image, 0)
-    # if args.brightness:
-    #     factor = 1.0 + random.uniform(-1.0*args.brightness, args.brightness)
-    #     table = np.array([((i / 255.0) * factor) * 255 for i in np.arange(0, 256)]).astype(np.uint8)
-    #     input_image = cv2.LUT(input_image, table)
-    # if args.rotation:
-    #     angle = random.uniform(-1*args.rotation, args.rotation)
-    #     M = cv2.getRotationMatrix2D((input_image.shape[1]//2, input_image.shape[0]//2), angle, 1.0)
-    #     input_image = cv2.warpAffine(input_image, M, (input_image.shape[1], input_image.shape[0]), flags=cv2.INTER_NEAREST)
-    #     output_image = cv2.warpAffine(output_image, M, (output_image.shape[1], output_image.shape[0]), flags=cv2.INTER_NEAREST)
+    if args.h_flip and random.randint(0,1):
+        input_image = cv2.flip(input_image, 1)
+        output_image = cv2.flip(output_image, 1)
+    if args.v_flip and random.randint(0,1):
+        input_image = cv2.flip(input_image, 0)
+        output_image = cv2.flip(output_image, 0)
+    if args.brightness:
+        factor = 1.0 + random.uniform(-1.0*args.brightness, args.brightness)
+        table = np.array([((i / 255.0) * factor) * 255 for i in np.arange(0, 256)]).astype(np.uint8)
+        input_image = cv2.LUT(input_image, table)
+    if args.rotation:
+        angle = random.uniform(-1*args.rotation, args.rotation)
+        M = cv2.getRotationMatrix2D((input_image.shape[1]//2, input_image.shape[0]//2), angle, 1.0)
+        input_image = cv2.warpAffine(input_image, M, (input_image.shape[1], input_image.shape[0]), flags=cv2.INTER_NEAREST)
+        output_image = cv2.warpAffine(output_image, M, (output_image.shape[1], output_image.shape[0]), flags=cv2.INTER_NEAREST)
 
     return input_image, output_image
 
